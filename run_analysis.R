081a0d20878fefca6e36edead515e5107e3145de
Simran <96426744+Sim2200@users.noreply.github.com>
2024-04-15 02:00:36 +0530

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
 library(dplyr)
 
+#loading dataset
 features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
@@ -9,15 +10,18 @@ subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names
 x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
+#merging dataset
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 Subject <- rbind(subject_train, subject_test)
 Merged_Data <- cbind(Subject, Y, X)
 
+#extracting only mean and sd measurement data
 TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
 
 TidyData$code <- activities[TidyData$code, 2]
 
+#naming the dataset
 names(TidyData)[2] = "activity"
 names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
 names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
@@ -32,6 +36,7 @@ names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRU
 names(TidyData)<-gsub("angle", "Angle", names(TidyData))
 names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
 
+#creating a new tidy dataset
 FinalData <- TidyData %>%
   group_by(subject, activity) %>%
   summarise_all(funs(mean))
